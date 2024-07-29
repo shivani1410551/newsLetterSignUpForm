12d56a8690973f85c27e62a04b2b0d9ef631e33c
shivani charan <sushikim2050@gmail.com>
2024-07-29 22:36:32 +0530

    change path

--- src/Components/ImageBlock.jsx
+++ src/Components/ImageBlock.jsx
@@ -2,12 +2,12 @@ const ImageBlock = () => {
   return (
     <div className="grid justify-self-center self-center sm:justify-self-end ">
       <img
-        src="/public/assets/images/illustration-sign-up-desktop.svg"
+        src="./assets/images/illustration-sign-up-desktop.svg"
         alt="desktop image"
         className="sm:block xs:hidden xxs:hidden sm:col-start-2 sm:h-[500px] "
       />
       <img
-        src="/public/assets/images/illustration-sign-up-mobile.svg"
+        src="./assets/images/illustration-sign-up-mobile.svg"
         alt="mobile image"
         className="sm:hidden   xxs:block xxs:w-[430px] xs:w-[450px] xs:h-[400px] 
         xxs:h-[480px]"
